8e08b8e72621e14806c31079f1f42fe45dbb1434
ZeKraken <zekraken00@gmail.com>
2023-04-02 19:23:05 -0400

    first update

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import { ethers } from 'ethers';
+import Button from '@mui/material/Button';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <>
+      <header className="headerContent">
       </header>
-    </div>
+      <br />
+      <div className="mainContent">
+      <Button variant="contained" color="primary">Create Pool</Button>
+      </div>
+    </>
   );
 }
 
